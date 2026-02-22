99a21800fa72ce731b6d073f15b3b167dbf5dced
Pablo-o-plomo <89897650120@mail.ru>
2026-02-22 14:44:20 +0300

    fix image recognition

--- vision.py
+++ vision.py
@@ -1,15 +1,15 @@
-from openai import OpenAI
+import base64
 import os
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def analyze_food(image_path):
 
-    with open(image_path, "rb") as f:
-        image_bytes = f.read()
+    with open(image_path, "rb") as img:
+        b64_image = base64.b64encode(img.read()).decode("utf-8")
 
     response = client.responses.create(
         model="gpt-4.1-mini",
@@ -17,11 +17,11 @@ def analyze_food(image_path):
             {
                 "role": "user",
                 "content": [
-                    {"type": "input_text", "text": "Определи что это за еда. Напиши: название блюда, примерные калории и БЖУ."},
+                    {"type": "input_text", "text": "Определи блюдо на фото. Напиши: название, калории (ккал), белки, жиры, углеводы."},
                     {
                         "type": "input_image",
-                        "image": image_bytes
-                    }
+                        "image_url": f"data:image/jpeg;base64,{b64_image}"
+                    },
                 ],
             }
         ],
